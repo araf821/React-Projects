080f7221d7698fcf743bdc84b0bfc78428833e40
araf821 <araf.ahmed200@gmail.com>
2023-02-06 22:37:08 -0500

    fetched data to be used

--- tours-project/src/App.js
+++ tours-project/src/App.js
@@ -4,6 +4,31 @@ import Tours from './Tours'
 
 const url = 'https://course-api.com/react-tours-project'
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const response = await fetch(url)
+        const tourList = await response.json()
+        setLoading(false)
+        console.log(tourList);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return <h2>Loading Tours...</h2>
+  }
+
+  if (error) {
+    return <h2>404 - Resource Not Found :(</h2>
+  }
+
   return <h2>Tours Project Setup</h2>
 }
 
