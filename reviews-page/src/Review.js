065551228fb28e09062f69f96edd1e9fa660a244
araf821 <araf.ahmed200@gmail.com>
2023-02-07 18:06:22 -0500

    set up the UI

--- reviews-page/src/Review.js
+++ reviews-page/src/Review.js
@@ -1,9 +1,47 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [reviews, setReviews] = useState(people);
+
+  const randomReview = reviews[Math.floor(Math.random() * reviews.length)];
+
+  return (
+    <main>
+      <div className="container">
+        {/* ----------TITLE-------- */}
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+
+        <article className="review">
+          {/* ---------- IMAGE CONTAINER ----------- */}
+          <div className="img-container">
+            <img
+              className="person-img"
+              src={randomReview.image}
+              alt={randomReview.name}
+            />
+            {/* ---------- QUOTATION ICON --------- */}
+            <div className="quote-icon">
+              <FaQuoteRight />
+            </div>
+          </div>
+
+          {/* REVIEW INFO */}
+          <h4 className="author">{randomReview.name}</h4>
+          <p className="job">{randomReview.job}</p>
+          <p className="info">{randomReview.text}</p>
+          
+          {/* PREV & NEXT BUTTONS */}
+          <FaChevronLeft className="prev-btn" />
+          <FaChevronRight className="next-btn" />
+        </article>
+      </div>
+    </main>
+  );
 };
 
 export default Review;
