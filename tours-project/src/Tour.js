8d7463d60a1b340e36d2bf2d0d78241f4eff7d5e
araf821 <araf.ahmed200@gmail.com>
2023-02-07 01:30:30 -0500

    added text shrink/expand functionality, improved the css

--- tours-project/src/Tour.js
+++ tours-project/src/Tour.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tour = ({ id, name, info, image, price, deleteItem }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <div className="single-tour" key={id}>
       <img src={image} alt={name} />
@@ -9,8 +11,17 @@ const Tour = ({ id, name, info, image, price, deleteItem }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">{price}</h4>
         </div>
-        <p>{info}</p>
-        <button className="delete-btn" onClick={() => deleteItem(id)}>Not Interested</button>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          {!readMore ? (
+            <button onClick={() => setReadMore(!readMore)}>&#8594;</button>
+          ) : (
+            <button onClick={() => setReadMore(!readMore)}>&#8592;</button>
+          )}
+        </p>
+        <button className="delete-btn" onClick={() => deleteItem(id)}>
+          Not Interested
+        </button>
       </footer>
     </div>
   );
