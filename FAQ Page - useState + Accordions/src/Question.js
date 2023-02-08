d85fe30f49112bf0aede728e22010c6735372f86
araf821 <araf.ahmed200@gmail.com>
2023-02-07 19:50:56 -0500

    completed project

--- FAQ Page - useState + Accordions/src/Question.js
+++ FAQ Page - useState + Accordions/src/Question.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, title, info }) => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <article key={id} className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setToggle(!toggle)}>
+          {toggle ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {toggle ? <p>{info}</p> : null}
+    </article>
+  );
 };
 
 export default Question;
