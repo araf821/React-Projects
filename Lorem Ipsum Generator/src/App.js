b81d54903b0e65ec36b761b3c385ece2bc54e11b
araf821 <araf.ahmed200@gmail.com>
2023-02-08 21:41:55 -0500

    implemented the form and UI

--- Lorem Ipsum Generator/src/App.js
+++ Lorem Ipsum Generator/src/App.js
@@ -1,7 +1,33 @@
 import React, { useState } from "react";
 import data from "./data";
+
 function App() {
-  return <h2>lorem ipsum project setup</h2>;
+  const [paragraphs, setParagraphs] = useState();
+
+  function handleSubmit(e) {
+    
+  }
+
+  return (
+    <div className="section-center">
+      <h3>Tired of boring old lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="paragraphs">Paragraphs:</label>
+        <input
+          type="number"
+          value={paragraphs}
+          defaultValue={1}
+          name="paragraphs"
+          id="paragraphs"
+          onChange={(e) => setParagraphs(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
