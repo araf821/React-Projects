c894f4a895255d593ad9f64184139fba7ec8a92a
araf821 <araf.ahmed200@gmail.com>
2023-02-09 21:13:08 -0500

    set up the basic layout of the project

--- Colour Generator/src/App.js
+++ Colour Generator/src/App.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
-
-import Values from 'values.js'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [colour, setColour] = useState();
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <section className="container">
+        <h3>Colour Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={colour}
+            onChange={(e) => setColour(e.target.value)}
+            placeholder="#bebebe"
+          />
+          <button className="btn">Get Colours!</button>
+        </form>
+      </section>
+      <section className="colors"></section>
+    </>
+  );
 }
 
-export default App
+export default App;
