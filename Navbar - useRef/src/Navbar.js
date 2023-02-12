d9f52e1868ff0519f073342a61b3c5af16974f90
araf821 <araf.ahmed200@gmail.com>
2023-02-11 20:21:46 -0500

    set up the basic UI

--- Navbar - useRef/src/Navbar.js
+++ Navbar - useRef/src/Navbar.js
@@ -1,10 +1,68 @@
 import React, { useState, useRef, useEffect } from "react";
-import { FaBars, FaTwitter } from "react-icons/fa";
+import { FaBars, FaTwitter, FaBehance, FaFacebook } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>;
+  return (
+    <>
+      <nav>
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} alt="" />
+            <button className="nav-toggle">
+              <FaBars />
+            </button>
+          </div>
+          <div
+            className="links-container show-container "
+            style={{ height: "0" }}
+          >
+            <ul className="links">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Projects</a>
+              </li>
+              <li>
+                <a href="#">Contacts</a>
+              </li>
+              <li>
+                <a href="#">Profile</a>
+              </li>
+            </ul>
+          </div>
+          <ul className="social-icons">
+            <li>
+              {" "}
+              <a href="facebook.com">
+                {" "}
+                <FaFacebook />{" "}
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="facebook.com">
+                {" "}
+                <FaTwitter />{" "}
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="facebook.com">
+                {" "}
+                <FaBehance />{" "}
+              </a>{" "}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
