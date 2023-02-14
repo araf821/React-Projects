19ae7a3e193e3d04e3cd564636e10a63d5fcd550
araf821 <araf.ahmed200@gmail.com>
2023-02-14 18:38:10 -0500

    set up our states and functions for modal and sidebar functionalities

--- Sidebar + Modal Components/src/components/Home.js
+++ Sidebar + Modal Components/src/components/Home.js
@@ -2,12 +2,11 @@ import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
 
 const Home = () => {
-
   return (
     <main>
-      <button className="btn"  >Show Modal</button>
+      <button className="btn">Show Modal</button>
       <button className="sidebar-toggle">
-        <FaBars/>
+        <FaBars />
       </button>
     </main>
   );
--- Sidebar + Modal Components/src/context.js
+++ Sidebar + Modal Components/src/context.js
@@ -3,7 +3,39 @@ import { useState, useContext, createContext } from "react";
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  const [showModal, setShowModal] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  function openModal() {
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
+  function openSidebar() {
+    setShowSidebar(true);
+  }
+
+  function closeSidebar() {
+    setShowSidebar(false);
+  }
+
+  return (
+    <AppContext.Provider
+      value={
+        (showModal,
+        showSidebar,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar)
+      }
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export { AppContext, AppProvider };
