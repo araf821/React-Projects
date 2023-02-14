f9fe60d92fdbe2af8add07915345733ec59878d6
araf821 <araf.ahmed200@gmail.com>
2023-02-14 18:32:27 -0500

    set up appProvider and appContext for passing data

--- Sidebar + Modal Components/src/App.js
+++ Sidebar + Modal Components/src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "./components/Modal";
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
+import { AppContext } from "./context";
 function App() {
+  const data = useContext(AppContext);
+
   return (
     <>
       <Home />
--- Sidebar + Modal Components/src/context.js
+++ Sidebar + Modal Components/src/context.js
@@ -1,1 +1,9 @@
-import React, { useState, useContext } from 'react'
+import { useState, useContext, createContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
+
+export { AppContext, AppProvider };
